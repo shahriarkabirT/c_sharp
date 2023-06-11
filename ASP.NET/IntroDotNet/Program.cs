93ee9d7608f8d334e572ccc0a0d8d88e1a397a5f
shahriar <shahriarkabir078@gmail.com>
2023-06-11 13:08:35 +0600

    new

---
+++ ASP.NET/IntroDotNet/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace IntroDotNet
+{
+    class Program
+    {
+        public static void Main()
+        {
+            int b; 
+            //string a = ".dot net";
+            //Console.WriteLine("Hello world Program for Nerd "+a+" Programmers");
+            //Console.WriteLine($"Hello world Program for Nerd  Programmers {a}");
+            //Console.WriteLine($"{a} -Hello world Program for Nerd");
+
+          //  Console.ReadKey();
+        }
+    }
+}
